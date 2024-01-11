096e4a55283a254aa145dfd9dc8fb55d2350fb64
Elliale <135648875+Elliale@users.noreply.github.com>
2024-01-11 13:27:45 +0100

    Delete chess directory

--- chess/piece.py
+++
@@ -1,9 +0,0 @@
-class Piece:
-    def __init__(self,position,destination,color):
-        self.position=position
-        self.destionation=destination
-        self.color=color
-    def Pawn():
-        pass
-
-
